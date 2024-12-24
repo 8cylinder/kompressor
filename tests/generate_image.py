1596d6d661c04b67730de14a350101f1ac1689e7
Sheldon McGrandle <developer@8cylinder.com>
2024-12-23 21:18:34 -0800

    Add type hints to test code

--- tests/generate_image.py
+++ tests/generate_image.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFilter
 import time
 
 
-def generate_test_image():
+def generate_test_image() -> Image.Image:
     image_size = (800, 600)
     num_circles = 50
     blur_radius = 20
@@ -11,7 +11,7 @@ def generate_test_image():
     # create a random rgb color
 
     # Create a new image with white background
-    rand_image = Image.new('RGB', image_size, "white")
+    rand_image = Image.new("RGB", image_size, "white")
     draw = ImageDraw.Draw(rand_image)
 
     # Generate and draw random circles
@@ -26,17 +26,27 @@ def generate_test_image():
     return rand_image
 
 
-def generate_circles(draw, image_size, num_circles):
+def generate_circles(
+    draw: ImageDraw.ImageDraw, image_size: tuple[int, int], num_circles: int
+) -> None:
     for _ in range(num_circles):
-        circle_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        circle_color = (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        )
         radius = random.randint(10, 100)  # Random radius between 10 and 100
-        position = (random.randint(0, image_size[0] - radius * 2),
-                    random.randint(0, image_size[1] - radius * 2))
-        draw.ellipse([position, (position[0] + radius * 2, position[1] + radius * 2)],
-                     fill=circle_color)
+        position = (
+            random.randint(0, image_size[0] - radius * 2),
+            random.randint(0, image_size[1] - radius * 2),
+        )
+        draw.ellipse(
+            [position, (position[0] + radius * 2, position[1] + radius * 2)],
+            fill=circle_color,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     image = generate_test_image()
     image.show()
     image.save(f"test_image_{int(time.time())}.png")
--- tests/test_condenser.py
+++ tests/test_condenser.py
@@ -7,20 +7,19 @@ from condenser.condenser import Compress
 import tempfile
 from pprint import pprint as pp
 import uuid
-from typeguard import typechecked
-from condenser.generate_image import generate_test_image
-
+from .generate_image import generate_test_image
+from typing import Generator
 
 # pytest
 # pytest -rP
 
+
 @pytest.fixture
-def runner():
+def runner() -> CliRunner:
     return CliRunner()
 
 
-@typechecked
-def test_humanize_formatting():
+def test_humanize_formatting() -> None:
     numbers: dict[str, int] = {
         "1 bytes": 1,
         "10 bytes": 10,
@@ -37,16 +36,15 @@ def test_humanize_formatting():
 
 
 @pytest.fixture
-@typechecked
-def sample_images_setup():
+def sample_images_setup() -> Generator[tuple[dict[str, Path], Path]]:
     with tempfile.TemporaryDirectory() as tmpdir:
-        image_id = str(uuid.uuid4()).split('-')[-1]
+        image_id = str(uuid.uuid4()).split("-")[-1]
         sample_images: dict[str, str] = {
-            f'sample-{image_id}.jpg': 'JPEG',
-            f'sample-{image_id}.png': 'PNG',
-            f'sample-{image_id}.webp': 'WEBP',
+            f"sample-{image_id}.jpg": "JPEG",
+            f"sample-{image_id}.png": "PNG",
+            f"sample-{image_id}.webp": "WEBP",
         }
-        sample_non_images: list[str] = [f'sample-{image_id}.txt']
+        sample_non_images: list[str] = [f"sample-{image_id}.txt"]
         created_files: dict[str, Path] = {}
 
         for image_name, image_format in sample_images.items():
@@ -57,28 +55,31 @@ def sample_images_setup():
 
         for non_image_name in sample_non_images:
             sample_non_image_path = Path(tmpdir) / non_image_name
-            with open(sample_non_image_path, 'w') as f:
-                f.write('This is a text file.')
-            created_files['TXT'] = sample_non_image_path
+            with open(sample_non_image_path, "w") as f:
+                f.write("This is a text file.")
+            created_files["TXT"] = sample_non_image_path
 
-        pp(['created_files:', created_files])
+        pp(["created_files:", created_files])
 
         yield created_files, Path(tmpdir)
 
 
-@typechecked
-def test_get_files_by_extension(sample_images_setup):
+def test_get_files_by_extension(
+    sample_images_setup: tuple[dict[str, Path], Path],
+) -> None:
     created_files, tmpdir = sample_images_setup
-    wanted_types: list[str] = ['xxx', 'jpg', 'jpeg', 'png', 'webp']
-    found_files: list[Path] = get_files_by_extension(tmpdir, wanted_types)
-    expected_files: list[Path] = [created_files[i] for i in created_files if i.lower() in wanted_types]
+    wanted_types: list[str] = ["xxx", "jpg", "jpeg", "png", "webp"]
+    found_files: list[Path] = get_files_by_extension(str(tmpdir), wanted_types)
+    expected_files: list[Path] = [
+        created_files[i] for i in created_files if i.lower() in wanted_types
+    ]
 
-    assert set(found_files) == set(expected_files), \
-        "The function should return the correct files matching the specified test_extensions"
+    assert (
+        set(found_files) == set(expected_files)
+    ), "The function should return the correct files matching the specified test_extensions"
 
 
-@typechecked
-def test_each_image_type(sample_images_setup):
+def test_each_image_type(sample_images_setup: tuple[dict[str, Path], Path]) -> None:
     """Test compression effectiveness across different image types.
 
     Iterates through a set of image types (JPEG, PNG, WEBP), compressing each
@@ -96,30 +97,33 @@ def test_each_image_type(sample_images_setup):
     - Correct output directory for the compressed image.
     - Reduced file size of the compressed image compared to the original.
     """
-    for image_type in ['JPEG', 'PNG', 'WEBP']:
+    for image_type in ["JPEG", "PNG", "WEBP"]:
         created_files, tmpdir = sample_images_setup
         source_image: Path = created_files[image_type]
         quality: int = 10
         image: Compress = Compress(source_image, quality)
         data = image.compress()
 
-        pp({
-            'source_image': data.source_image,
-            'output_dir': image.output_dir,
-            'compressed_image': data.compressed_image,
-            'source size': data.original_size,
-            'compressed size': data.compressed_size,
-        })
+        pp(
+            {
+                "source_image": data.source_image,
+                "output_dir": image.output_dir,
+                "compressed_image": data.compressed_image,
+                "source size": data.original_size,
+                "compressed size": data.compressed_size,
+            }
+        )
 
         assert data.compressed_image.exists(), "The compressed image should exist."
-        assert data.compressed_image.parent.parts[-1] == "condensed", \
-            "The compressed image should be created in the specified output directory."
-        assert data.compressed_size < data.original_size, \
-            "The compressed image should have a smaller file size than the original image."
+        assert (
+            data.compressed_image.parent.parts[-1] == "condensed"
+        ), "The compressed image should be created in the specified output directory."
+        assert (
+            data.compressed_size < data.original_size
+        ), "The compressed image should have a smaller file size than the original image."
 
 
-@typechecked
-def test_rename(sample_images_setup):
+def test_rename(sample_images_setup: tuple[dict[str, Path], Path]) -> None:
     """Test the renaming functionality for both source and destination images
     during compression.
 
@@ -133,35 +137,41 @@ def test_rename(sample_images_setup):
     """
     created_files, tmpdir = sample_images_setup
 
-    source_image: Path = created_files['JPEG']
+    source_image: Path = created_files["JPEG"]
     quality: int = 50
     image: Compress = Compress(source_image, quality)
 
-    subdir: str = 'alternative-output-dir'
+    subdir: str = "alternative-output-dir"
     image.output_dir = Path(subdir)
 
-    source_rename: str = '-SOURCE-RENAMED'
+    source_rename: str = "-SOURCE-RENAMED"
     image.source_rename = source_rename
 
-    dest_rename: str = '-DEST-RENAMED'
-    image.dest_rename = dest_rename
+    dest_rename: str = "-DEST-RENAMED"
+    image.dest_extra_name = dest_rename
 
     data = image.compress()
 
-    pp({
-        'source_image': data.source_image,
-        'output_dir': image.output_dir,
-        'compressed_image': data.compressed_image,
-        'source size': data.original_size,
-        'compressed size': data.compressed_size,
-    })
+    pp(
+        {
+            "source_image": data.source_image,
+            "output_dir": image.output_dir,
+            "compressed_image": data.compressed_image,
+            "source size": data.original_size,
+            "compressed size": data.compressed_size,
+        }
+    )
 
     assert data.compressed_image.exists(), "The compressed image should exist."
-    assert data.compressed_image.parent.parts[-1] == subdir, \
-        "The compressed image should be created in the specified output directory."
-    assert data.compressed_size < data.original_size, \
-        "The compressed image should have a smaller file size than the original image."
-    assert source_rename in data.source_image.name, \
-        "The source image should have the specified source rename in its name."
-    assert dest_rename in data.compressed_image.name, \
-        "The compressed image should have the specified destination rename in its name."
+    assert (
+        data.compressed_image.parent.parts[-1] == subdir
+    ), "The compressed image should be created in the specified output directory."
+    assert (
+        data.compressed_size < data.original_size
+    ), "The compressed image should have a smaller file size than the original image."
+    assert (
+        source_rename in data.source_image.name
+    ), "The source image should have the specified source rename in its name."
+    assert (
+        dest_rename in data.compressed_image.name
+    ), "The compressed image should have the specified destination rename in its name."
